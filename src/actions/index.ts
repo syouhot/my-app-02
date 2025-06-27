cad007917b9c098412d2c73ca26065d5cd7a0f94
cuiyuesheng <syouhotcys@yeah.net>
2025-06-27 17:02:02 +0800

    1

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,33 +21,46 @@ export async function editSnippet(id: number, code: string) {
 
     redirect('/snippets/' + id)
 }
-export async function createSnippet(prevState: { message: string,payLoad:FormData }, formData: FormData) {
+export async function createSnippet(prevState: { message: string, payLoad: FormData }, formData: FormData) {
     'use server'
-    const title = formData.get('title') as string
-    const code = formData.get('code') as string
+    try {
+        // throw new Error("error")
+        const title = formData.get('title') as string
+        const code = formData.get('code') as string
 
-    if (typeof title !== 'string' || title.length < 3) {
-        return {
-            ...prevState,
-            message: "title is too short",
-            payLoad: formData
+        if (typeof title !== 'string' || title.length < 3) {
+            return {
+                ...prevState,
+                message: "title is too short",
+                payLoad: formData
+            }
+        }
+        if (typeof code !== 'string' || code.length < 3) {
+            return {
+                ...prevState,
+                message: "code is too short",
+                payLoad: formData
+            }
+        }
+
+        const sinppet = await db.snippet.create({
+            data: {
+                title,
+                code
+            }
+        })
+        console.log(sinppet);
+        //这个是用在服务端组件的跳转方法
+    } catch (err) {
+        if (err instanceof Error) {
+            return {
+                message: err.message
+            }
+        } else {
+            return {
+                message: "unknown error"
+            }
         }
     }
-    if (typeof code !== 'string' || code.length < 3) {
-        return {
-            ...prevState,
-            message: "code is too short",
-            payLoad: formData
-        }
-    }
-
-    const sinppet = await db.snippet.create({
-        data: {
-            title,
-            code
-        }
-    })
-    console.log(sinppet);
-    //这个是用在服务端组件的跳转方法
     redirect("/")
 }
